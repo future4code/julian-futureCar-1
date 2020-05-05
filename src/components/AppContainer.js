559bb1b2a3b40db3c60c1d68d9096334a55ef289
Leonardo <leonardolima2008@gmail.com>
2020-05-05 16:17:07 -0300

    Footer concluido

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
-
+import Footer from './Footer'
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+              <Footer />
       </div>
     )
   }
