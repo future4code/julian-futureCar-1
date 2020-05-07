e773a0d334a3b891dacc66f72fcc7515ef8756b5
Melissa Tammy <mel.tammy2@gmail.com>
2020-05-07 15:30:13 -0300

    Header e pagina corrigidos

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './AppContainer.css'
 import styled from 'styled-components';
-import Banner from '../imagem/carro.jpg';
+import Banner from '../imagem/banner-compro-carro.svg';
+import Poligono from '../imagem/poligono.png'
 import { Menu } from './Menu'
 import { CardContainer } from './CardContainer'
 import { Header } from './Header'
@@ -18,11 +19,19 @@ const Container = styled.div`
   max-width:100vw;
   margin:0;
 `
+
+const MenuContainer = styled.div`
+display:block;
+position:absolute;
+margin-top:48vw;
+`
+
 const SectionProdutos = styled.section`
   max-width:100%;
   display:flex;
   flex-direction: row;
   margin:0;
+  margin-top:1vw;
 `
 
 
@@ -30,36 +39,77 @@ const Main = styled.main`
   display:flex;
   align-items:center;
   flex-direction: column;
-  max-width:99vw;
+  max-width:100vw;
 `
 
 const BannerImg = styled.img`
 width:100%;
 `
 
+const Titulo = styled.img`
+  margin-top:10vw;
+  margin-left:2vw;
+  align-self: flex-start;
+  width:30vw;
+  height: 3vw;
+`
+
+
 export class AppContainer extends React.Component {
 
   state = {
-
+    trocaPagina: false
   }
+
+    onClickTrocaPagina = () => {
+      this.setState({
+        trocaPagina: true
+      })
+    }
 
 
   render() {
+
+
+
     return (
-      <Container>
-          <Header />
-        <BannerImg src={Banner} />
-        <Main>
-          <Menu />
-          <SectionProdutos>
-            <CardContainer />
-          </SectionProdutos>
-        </Main>
+      <div>
+
+        {
+          this.state.trocaPagina ? (
+
+            <Cadastro></Cadastro>
+
+          ) : (
+
+              <Container>
+                <Header onClickVender={this.onClickTrocaPagina} />
+                <BannerImg src={Banner} />
+                <MenuContainer>
+                  <Menu />
+                </MenuContainer>
+                <Main>
+                  <Titulo src={Poligono} />
+                  <SectionProdutos>
+                    <CardContainer />
+                  </SectionProdutos>
+                </Main>
+                <Footer />
+              </Container>
+
+            )
+        }
 
 
 
-        <Footer />
-      </Container>
+
+
+
+
+      </div>
+
+
+
 
     )
   }
