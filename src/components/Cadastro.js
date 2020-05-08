de3bcb891a57b3bc07ed5734926be6e17eedf3a3
Melissa Tammy <mel.tammy2@gmail.com>
2020-05-08 16:58:45 -0300

    cadastro de carros ok

--- src/components/Cadastro.js
+++ src/components/Cadastro.js
@@ -1,21 +1,20 @@
-import React from 'react'
-import styled from 'styled-components'
-import logo from '../components/imagens/futurecar.png'
-import logocar from '../components/imagens/logo.png'
-import carimagem from '../components/imagens/foto-100.jpg'
-
+import React from "react";
+import styled from "styled-components";
+import logo from "../components/imagens/futurecar.png";
+import logocar from "../components/imagens/logo.png";
+import carimagem from "../components/imagens/foto-100.jpg";
+import axios from "axios";
 
 const Car = styled.img`
   margin: 0;
   padding: 0;
   z-index: 1;
   width: 100%;
-  opacity: 0.2;
+  opacity: 0.1;
   position: fixed;
-`
+`;
 
 const Body = styled.div`
-  
   background: #f2ce46;
   margin: 0;
   padding: 0;
@@ -26,7 +25,7 @@ const Body = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 2;
-`
+`;
 
 const Form = styled.div`
   display: grid;
@@ -34,25 +33,25 @@ const Form = styled.div`
   align-items: center;
   margin: 0;
   padding: 0;
-  width: 35vw;
-  height: 80vh;
+  width: 40vw;
+  height: 120vh;
   background: white;
   border-radius: 20px;
   margin: auto;
   z-index: 1;
-`
+`;
 
 const Informacao = styled.div`
   color: red;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   font-weight: 700;
   z-index: 1;
-`
+`;
 
 const Texto = styled.div`
   margin-top: 70px;
-  margin-bottom: 120px; 
-  width:100%;
+  margin-bottom: 120px;
+  width: 100%;
   color: red;
   font-family: sans-serif;
   text-align: center;
@@ -60,21 +59,27 @@ const Texto = styled.div`
   font-size: 1.5rem;
   justify-content: center;
   z-index: 1;
-`
+`;
 
 const InputDiv = styled.div`
-font-family: 'Montserrat', sans-serif;
-font-weight: 700;
-display: grid;
-
-`
+  font-family: "Montserrat", sans-serif;
+  font-weight: 700;
+  display: grid;
+`;
 
 const Input = styled.input`
   border: none;
-  background: rgb(248,250,248);
+  background: rgb(248, 250, 248);
   width: 100%;
   height: 42px;
-`
+`;
+
+const TextArea = styled.input`
+  border: none;
+  background: rgb(248, 250, 248);
+  width: 100%;
+  height: 120px;
+`;
 
 const Button = styled.button`
   width: 260px;
@@ -83,7 +88,7 @@ const Button = styled.button`
   border: none;
   font-size: 1.3rem;
   margin: auto;
-`
+`;
 
 const Footer = styled.div`
   width: 100%;
@@ -95,104 +100,145 @@ const Footer = styled.div`
   justify-content: center;
   align-items: center;
   z-index: 1;
-`
+`;
 
 const Imagem = styled.img`
   height: 100%;
-`
+`;
 
 const Img = styled.img`
   width: 40vw;
   height: 40vh;
   margin: auto;
   z-index: 1;
-`
-
-
-
+`;
 
 export class Cadastro extends React.Component {
   state = {
     nome: "",
-    email:"",
-    telefone:"",
-    senha: ""
-  }
+    descricao: "",
+    preco: "",
+    url: "",
+    prazo: ""
+  };
+
 
   onChangeNome = (event) => {
-    this.setState({nome : event.target.value})
-  } 
-  onChangeEmail = (event) => {
-    this.setState({email : event.target.value})
-  } 
-  onChangeTelefone = (event) => {
-    this.setState({ telefone: event.target.value })
-  } 
-  onChangeSenha = (event) => {
-    this.setState({ senha : event.target.value })
-  } 
+    this.setState({ nome: event.target.value });
+  };
 
-  render () {
+  onChangeDescricao = (event) => {
+    this.setState({ descricao: event.target.value });
+  };
+
+  onChangePreco = (event) => {
+    this.setState({ preco: event.target.value });
+  };
+
+  onChangeUrl = (event) => {
+    this.setState({ url: event.target.value });
+  };
+
+  onChangePrazo = (event) => {
+    this.setState({ prazo: event.target.value });
+  };
+
+  onClickSubmit = () => {
+    this.cadastraCarro(this.state.nome, this.state.descricao, this.state.preco, this.state.url, this.state.prazo)
+  }
+
+  cadastraCarro = (InputNome, InputDescricao, InputPreco, InputUrl, InputPrazo) => {
+    const body = {
+      name: InputNome,
+      description: InputDescricao,
+      price: InputPreco,
+      paymentMethod: InputUrl,
+      shipping: InputPrazo
+    }
+    console.log(body)
+    axios
+      .post(
+        "https://us-central1-labenu-apis.cloudfunctions.net/futureCarOne/cars",
+        body,
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      .then((response) => {
+        console.log("eeee funcionou");
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  };
+
+  render() {
     return (
-        <Body>
+      <Body>
+        <Car src={carimagem}></Car>
 
-          <Car src={carimagem}></Car>
-
-          <Img src={logocar}></Img>
-          <Texto>
-          <h1>ÓTIMA ESCOLHA EM VENDER SEU <br></br>CARRO CONOSCO</h1>
-          </Texto>
+        <Img src={logocar}></Img>
+        <Texto>
+          <h1>
+            ÓTIMA ESCOLHA EM VENDER SEU <br></br>CARRO CONOSCO
+          </h1>
+        </Texto>
 
         <Form>
           <Informacao>
-            <h2>PREENCHA SEUS DADOS</h2>  
+            <h2>PREENCHA SEUS DADOS</h2>
           </Informacao>
 
           <InputDiv>
-          <label>*NOME </label>
-          <Input
-          type="text"
-          placeholder="Nome"
-          onChange={this.onChangeNome}                   
-          />
+            <label>*NOME/MARCA </label>
+            <Input
+              type="text"
+              placeholder="Nome"
+              onChange={this.onChangeNome}
+            />
           </InputDiv>
 
-      <InputDiv>
-        <label>*EMAIL</label>
-        <Input
-          type="text"
-          placeholder="E-mail"
-          onChange={this.onChangeEmail}     
-        />
-      </InputDiv>
+          <InputDiv>
+            <label>*DESCRIÇÃO</label>
+            <TextArea onChange={this.onChangeDescricao} />
+          </InputDiv>
 
-      <InputDiv>
-      <label>*TELEFONE</label>
-        <Input
-          type="text"
-          placeholder="Telefone"
-          onChange={this.onChangeTelefone}     
-        />
-        </InputDiv>
+          <InputDiv>
+            <label>*PREÇO</label>
+            <Input
+              type="number"
+              placeholder="preço"
+              onChange={this.onChangePreco}
+            />
+          </InputDiv>
 
-        <InputDiv>
-        <label>*TELEFONE</label>
-        <Input
-          type="text"
-          placeholder="Telefone"
-          onChange={this.onChangeSenha}     
-        />
-        </InputDiv>
+          <InputDiv>
+            <label>*URL DA IMAGEM</label>
+            <Input type="text" onChange={this.onChangeUrl} />
+          </InputDiv>
 
-        <Button>Enviar</Button>
+          <InputDiv>
+            <label>*PRAZO DE ENTREGA</label>
+            <Input type="text" onChange={this.onChangePrazo} />
+          </InputDiv>
+
+
+          <Button
+            onClick={this.onClickSubmit}>
+            Enviar
+          </Button>
         </Form>
 
         <Footer>
           <Imagem src={logo}></Imagem>
-          <p>©Todos os direitos reservados a Shirlei, Melissa, Leonardo e Wellington</p>
+          <p>
+            ©Todos os direitos reservados a Shirlei, Melissa, Leonardo e
+            Wellington
+          </p>
         </Footer>
-        </Body>
-
-    )
+      </Body>
+    );
   }
 }
